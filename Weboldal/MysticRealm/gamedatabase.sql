16a49cb310d7070554b07545d01a3e16e0a63987
SzilagyiZsolt <zsoltika20030@gmail.com>
2024-04-25 08:08:20 +0200

    Vizsga remek

--- Weboldal/MysticRealm/gamedatabase.sql
+++ Weboldal/MysticRealm/gamedatabase.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `gamedatabase`
 --
-
+CREATE DATABASE IF NOT EXISTS `gamedatabase` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `gamedatabase`;
 -- --------------------------------------------------------
 
 --
